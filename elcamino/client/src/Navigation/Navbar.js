ce2aa9b27036bfbacd8e69b536cdc7ffa320d7e3
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2022-09-30 16:26:49 -0400

    Updated Navbar

--- elcamino/client/src/Navigation/Navbar.js
+++ elcamino/client/src/Navigation/Navbar.js
@@ -1,16 +1,16 @@
 import '../App.css';
-import clayconn from '../Graphics/clayconn.svg';
+import claytonwhite from '../Graphics/claytonwhite.svg';
 
 export  function Navbar() {
   return (
     <div className="Navbar">
       <div>
         <a href='https://claytoncountyga.gov'>
-          <img src={clayconn} alt='County Logo' />
+          <img src={claytonwhite} alt='County Logo' />
         </a>
       </div>
       <ul className="menuItems">
-      <li><a href="#">Clayton County Permitting</a></li>
+      {/* <li><a href="#">Clayton County Permitting</a></li> */}
       <li><a href="https://claytoncountyga.gov/">Go to Claytoncountyga.gov</a></li>
       <li></li>
       <li></li>

f1c5e97d98a0134ee133a96dd9f803905b84fffd
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2022-10-10 21:34:34 -0400

    Updated queries.js to work on the server

--- elcamino/server/queries.js
+++ elcamino/server/queries.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'node',
-  host: 'doitwebdev01',
+  host: 'localhost',
   database: 'node',
   password: 'Cl@yt0n42!',
   port: 5432,

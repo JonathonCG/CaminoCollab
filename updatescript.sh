33a7a539a2d032b7bf84cf3173c329c9365741c7
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2022-12-29 13:00:38 -0500

    Finished up the update script (^-^)

--- updatescript.sh
+++ updatescript.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+# ** THIS SCRIPT MUST BE RUN WITH sudo **
+# It shuts down the containers, removes the client image
 
-# Run 'docker compose down' to shutdown the containers
-echo "!! Shutting Down Containers !!"
+echo "** Shutting Down Containers **"
 docker compose down
 
-echo "!! Removing caminocollab-client Image !!"
+echo "** Removing caminocollab-client Image **"
 docker rmi caminocollab-client
 
-echo "!! Pulling Changes from Git !!"
+echo "** Pulling Changes from Git **"
 git pull
 
+echo "** Initializing the Docker Containers Again **"
+docker compose up -d
+
+echo "(^-^) Everything should be updated and running normally now! Congratulations!"

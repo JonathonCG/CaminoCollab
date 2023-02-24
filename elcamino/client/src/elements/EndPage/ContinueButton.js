02cb154464d0a4d2be75ac2e00e387dd4af809a0
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2023-02-24 17:53:20 -0500

    fixed continue button

--- elcamino/client/src/elements/EndPage/ContinueButton.js
+++ elcamino/client/src/elements/EndPage/ContinueButton.js
@@ -1,19 +1,19 @@
 
-import { useParams } from "react-router-dom";
 import { Button } from "@nextui-org/react";
 
-export function ContinueButton({selected}, {cardlink}) { /* Function changes what is displayed in the continue button element based on if "ischecked" is true or false */
-const { id } = useParams();
+export function ContinueButton({selected, cardlink}) { /* Function changes what is displayed in the continue button element based on if "ischecked" is true or false */
     if(selected){
         return (
-            <Button as="a" href={cardlink} size="lg" shadow color="gradient">
+            <a href={cardlink}>
+            <Button size="lg" shadow color="gradient">
                 Continue to the Permit Application
             </Button>
+            </a>
         )
         }
         return (
             <Button size="lg" disabled>
                 Continue to the Permit Application
             </Button>
-        )
+        );
 }

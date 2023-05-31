c4573645df584e5ed2d7d4d0a60537e3140de331
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2023-05-31 16:06:30 -0400

    Updated Google Analytics Implementation

--- elcamino/client/src/App.js
+++ elcamino/client/src/App.js
@@ -4,17 +4,12 @@ import { Home } from "./pages/Home";
 import { Assistant } from "./pages/Assistant";
 import { EndPage } from './pages/EndPage';
 import ScrollToTop from './elements/Scrolltotop';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 // Google Analytics Initialization
-const trackingId = "G-N6T9KG86GQ";
-ReactGA.initialize(trackingId);
+ReactGA.initialize('G-N6T9KG86GQ');
 
 function App() {
-  // Google Analytics Page Tracking
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
 
   return (
     <ScrollToTop>

9ae486409ef7e52cb962629580c58d6270363464
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2023-05-31 15:27:17 -0400

    Added Google Analytics

--- elcamino/client/src/App.js
+++ elcamino/client/src/App.js
@@ -1,11 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Assistant } from "./pages/Assistant"
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Assistant } from "./pages/Assistant";
 import { EndPage } from './pages/EndPage';
 import ScrollToTop from './elements/Scrolltotop';
+import ReactGA from 'react-ga';
 
+// Google Analytics Initialization
+const trackingId = "G-N6T9KG86GQ";
+ReactGA.initialize(trackingId);
 
 function App() {
+  // Google Analytics Page Tracking
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <ScrollToTop>
       <Routes>

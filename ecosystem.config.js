60c96fa9268cf9d0733c2e73bdc32f80caeaa85b
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2023-08-03 15:50:50 -0400

    tweaked pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
     apps : [{
         name: "assistant",
-        script: "serve ./elcamino/client/build",
-        args: '--spa',
+        script: "serve",
+        cwd: './elcamino/client',
         watch: true,
         env: {
-            PM2_SERVE_PATH: '.',
-            PM2_SERVE_PORT: 3000
+            PM2_SERVE_PATH: './elcamino/client/build',
+            PM2_SERVE_PORT: 3000,
+            PM2_SERVE_SPA: 'true'
         }
       }, {
         name: "api",
         script: "./elcamino/server/index.js",
+        cwd: "./elcamino/server",
         watch: true,
         env: {
             NODE_ENV: "production",
